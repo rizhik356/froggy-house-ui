28585d6236e222f03ecfb100803ee9d6efeb5777
makhanek <makhanek@infocom.dev>
2025-09-16 16:44:14 +0300

    upd dockerfile

--- src/modules/Header/ui/Header.tsx
+++ src/modules/Header/ui/Header.tsx
@@ -4,8 +4,11 @@ import {
   IconButton,
   Typography,
   Box,
-  Tooltip,
   Avatar,
+  Menu,
+  MenuItem,
+  ListItemText,
+  ListItemIcon,
 } from '@mui/material'
 
 import MenuIcon from '@mui/icons-material/Menu'
@@ -14,10 +17,24 @@ import { useNavigate } from 'react-router-dom'
 import { paths } from '../../../constants'
 import { useDispatch } from 'react-redux'
 import { headerActions } from '../model/slices/headerSlice.ts'
+import { useState } from 'react'
+
+import SettingsIcon from '@mui/icons-material/Settings'
+import LogoutIcon from '@mui/icons-material/Logout'
+import { logOut } from '../../../shared/utils/logOut.ts'
 
 const Header = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
   const navigate = useNavigate()
   const dispatch = useDispatch()
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
 
   return (
     <AppBar position={'sticky'}>
@@ -50,13 +67,39 @@ const Header = () => {
           </Typography>
         </Box>
         <Box sx={{ flexGrow: 0 }}>
-          <Tooltip title="Open settings">
-            <IconButton sx={{ p: 0 }}>
-              <Avatar>
-                <PersonIcon />
-              </Avatar>
-            </IconButton>
-          </Tooltip>
+          <IconButton sx={{ p: 0 }} onClick={handleMenu}>
+            <Avatar>
+              <PersonIcon />
+            </Avatar>
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose}>
+              <ListItemIcon>
+                <SettingsIcon fontSize="small" />
+              </ListItemIcon>
+              <ListItemText>Настройки</ListItemText>
+            </MenuItem>
+            <MenuItem onClick={logOut}>
+              <ListItemIcon>
+                <LogoutIcon fontSize="small" />
+              </ListItemIcon>
+              <ListItemText>Выход</ListItemText>
+            </MenuItem>
+          </Menu>
         </Box>
       </Toolbar>
     </AppBar>
--- src/shared/utils/logOut.ts
+++ src/shared/utils/logOut.ts
@@ -1,8 +1,10 @@
 import { store } from '../../app/store'
 import { authActions } from '../../modules/Auth'
+import { paths } from '../../constants'
 
 const logOut = () => {
   store.dispatch(authActions.clearAuthData())
+  window.location.href = paths.AUTH.LOGIN
 }
 
 export { logOut }
