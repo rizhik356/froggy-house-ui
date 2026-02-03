d8e73d6aca4ecbbc45a2d8f9c9e1304e2690d38d
Dmitry <dmitry@MacBook-Pro-Dmitry.local>
2026-02-03 22:36:36 +0300

    chng ghead req

--- src/modules/Devices/model/api/getNewDeviceHead.ts
+++ src/modules/Devices/model/api/getNewDeviceHead.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const getNewDeviceHead = async () => {
-  return await axios.head(import.meta.env.VITE_NEW_DEVICE_URL, {
+  return await axios.get(import.meta.env.VITE_NEW_DEVICE_URL, {
     timeout: 3000,
     headers: {
       'Access-Control-Request-Private-Network': 'true',
