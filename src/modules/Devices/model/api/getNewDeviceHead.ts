2d149a471ea395ba0dba42eaac9497546354cf86
Dmitry <dmitry@MacBook-Pro-Dmitry.local>
2026-02-03 19:32:55 +0300

    chng ghead req

--- src/modules/Devices/model/api/getNewDeviceHead.ts
+++ src/modules/Devices/model/api/getNewDeviceHead.ts
@@ -1,9 +1,16 @@
+const getNewDeviceHead = (): Promise<void> => {
+  return new Promise((resolve, reject) => {
+    try {
+      const success = navigator.sendBeacon(import.meta.env.VITE_NEW_DEVICE_URL)
 
-
-const getNewDeviceHead = async () => {
-  return await fetch(import.meta.env.VITE_NEW_DEVICE_URL, {
-    method: 'HEAD',
-    mode: "no-cors"
+      if (success) {
+        resolve() // Успешно отправлено
+      } else {
+        reject(new Error('sendBeacon failed')) // Не удалось отправить
+      }
+    } catch (error) {
+      reject(error) // Ошибка при вызове
+    }
   })
 }
 
